d84225b9b0ba34f84f9f790a4190b4dbbf49ecb1
S.Zapf <nc-zapfsa@netcologne.de>
2024-07-01 15:03:27 +0200

    safe

--- codewars/python/active/schmierzettel.py
+++ codewars/python/active/schmierzettel.py
@@ -1,6 +1,11 @@
-rs = [3,5,7,9]
+def group_in_10s(*args):
+    res = [None] * ((max(args))//10+1)
+    for n in args:
+        i = n//10
+        if res[i] == None:
+            res[i] = []
+        res[i].append(n)
+    return [sorted(a) if a else None for a in res]
 
-for r in rs:
-    ratio = ((2*(r**2))**0.5)/r
-    print(ratio)
+print(group_in_10s(8, 12, 38, 3, 17, 19, 25, 35, 50))
           
