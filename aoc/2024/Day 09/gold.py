a880568b13527a07fc3fc249a551980a99db4742
Sascha Zapf <nc-zapfsa@netcologne.de>
2024-12-09 23:10:39 +0100

    safe

--- aoc/2024/Day 09/gold.py
+++ aoc/2024/Day 09/gold.py
@@ -13,67 +13,54 @@ def gen_disk(disk_map):
     map_list = list(disk_map)
     disk = []
     while map_list:
-        disk += [[id] * int(map_list.pop(0))]
+        disk.append((id,int(map_list.pop(0))))
         id += 1
         if map_list:
             c = int(map_list.pop(0))
             if c:
-                disk += [[-1] * c]
-    return disk    
-    
-def insert_file(spacer, file):
-    if len(spacer) == len(file):
-        return file
-    else:
-        return file, spacer[len(file):]    
-    
-disk_map = load("showcase 01.txt")
-disk = gen_disk(disk_map)
+                disk.append((-1, c)) 
+    return id-1,disk    
 
+def find_id_idx(disk, id):
+    right = len(disk)-1
+    while disk[right][0] != id: right -= 1
+    return right
+
+def find_space_idx(disk, size, right):
+    for idx in range(0,right):
+        if disk[idx][0] == -1 and disk[idx][1] >= size:
+            return idx
+    return 0    
+
+disk_map = load("puzzle.txt")
+max_id, disk = gen_disk(disk_map)
+
+print(max_id, disk)
+
+right_idx = len(disk)
 print(disk)
-free_blocks =[(idx,len(ids)) for idx, ids in enumerate(disk) if ids[0] == -1 ]
-print(free_blocks)
+for id in range(max_id,0,-1):
+    right_idx = find_id_idx(disk,id)
+    space_idx = find_space_idx(disk,disk[right_idx][1] ,right_idx)
+    if space_idx > 0:
+        print(id,space_idx, right_idx)
+        disk[right_idx] = (-1,disk[right_idx][1])
+        disk[space_idx] = (id,disk[space_idx][1])
+        free = disk[space_idx][1] - disk[right_idx][1]
+        if free:
+            disk[space_idx] = (disk[space_idx][0],disk[right_idx][1])
+            disk.insert(space_idx+1, (-1, free))
 
-sys.exit()
-
-right_idx = len(disk)-1
-
-while right_idx:
-    if disk[right_idx][0] == -1:
-        right_idx -= 1
-        continue
-    else:
-        for left_idx in range(right_idx):
-            if disk[left_idx][0] > 0:
-                continue 
-            if len(disk[left_idx]) >= len(disk[right_idx])    
-        
-        
-        else:
-            
-
-
-        if disk[e_idx][0] == -1 and len(disk[e_idx]) >= len(disk[idx]):
-
-
-
-
-while free_blocks:
-    #print(disk,free_blocks)
-
-    id = disk.pop()
-    if id != -1:
-        free_idx = free_blocks.pop(0)
-        if free_idx < len(disk):
-            disk[free_idx] = id
-    else:
-        free_blocks.pop()
-
-#print(disk,free_blocks)
-
+print(disk)   
+defrag = []
+for id,size in disk:
+    if id == -1:
+        id = 0
+    defrag.extend([id] * size)
+print(defrag)  
 
 chksum = 0
-for idx,id in enumerate(disk):
-    chksum += idx * id
+for idx,id in enumerate(defrag):
+    chksum += idx*id
 
-print(chksum)
+print(chksum)    
